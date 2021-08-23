218b8157a20f876fe6e81f4d7e98753e7ad531a9
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2021-08-23 14:33:17 +0300

    WIP: Use person properties in trends/funnels

--- ee/clickhouse/queries/funnels/funnel_event_query.py
+++ ee/clickhouse/queries/funnels/funnel_event_query.py
@@ -7,7 +7,6 @@ from posthog.constants import TREND_FILTER_TYPE_ACTIONS
 
 class FunnelEventQuery(ClickhouseEventQuery):
     def get_query(self, entities=None, entity_name="events", skip_entity_filter=False) -> Tuple[str, Dict[str, Any]]:
-        column_optimizer = ColumnOptimizer(self._filter, self._team_id)
         _fields = (
             f"{self.EVENT_TABLE_ALIAS}.event as event, "
             + f"{self.EVENT_TABLE_ALIAS}.team_id as team_id, "
@@ -15,23 +14,33 @@ class FunnelEventQuery(ClickhouseEventQuery):
             + f"{self.EVENT_TABLE_ALIAS}.timestamp as timestamp"
             + (
                 f", {self.EVENT_TABLE_ALIAS}.properties as properties"
-                if column_optimizer.should_query_event_properties_column
+                if self._column_optimizer.should_query_event_properties_column
                 else ""
             )
             + (
                 f", {self.EVENT_TABLE_ALIAS}.elements_chain as elements_chain"
-                if column_optimizer.should_query_elements_chain_column
+                if self._column_optimizer.should_query_elements_chain_column
                 else ""
             )
-            + (f", {self.DISTINCT_ID_TABLE_ALIAS}.person_id as person_id" if self._should_join_distinct_ids else "")
-            + (f", {self.PERSON_TABLE_ALIAS}.person_props as person_props" if self._should_join_persons else "")
             + (
                 " ".join(
-                    [
-                        f", {self.EVENT_TABLE_ALIAS}.{column_name} as {column_name}"
-                        for column_name in column_optimizer.materialized_event_columns_to_query
-                    ]
+                    f", {self.EVENT_TABLE_ALIAS}.{column_name} as {column_name}"
+                    for column_name in self._column_optimizer.materialized_event_columns_to_query
                 )
+            )
+            + (f", {self.DISTINCT_ID_TABLE_ALIAS}.person_id as person_id" if self._should_join_distinct_ids else "")
+            + (
+                f", {self.PERSON_TABLE_ALIAS}.person_props as person_props"
+                if self._should_join_persons and self._column_optimizer.should_query_person_properties_column
+                else ""
+            )
+            + (
+                " ".join(
+                    f", {self.PERSON_TABLE_ALIAS}.{column_name} as {column_name}"
+                    for column_name in self._column_optimizer.materialized_person_columns_to_query
+                )
+                if self._should_join_persons
+                else ""
             )
         )
 
